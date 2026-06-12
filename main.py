78d03a8646aead260e658c8eca8f7d6cf3d3bead
rajeshsamal745 <rajeshsamal745@gmail.com>
2026-06-12 16:28:03 +0530

    Hotflix v4.1: Improved division by zero error message

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    return a / b if b != 0 else "Error: Division by zero"
+
 if __name__ == "__main__":
     print("Addition Example:", add(5, 3))
     print("Subtraction Example:", subtract(5, 3))
     print("Multiplication Example:", multiply(5, 3))
+    print("Division Example:", divide(5, 0))
